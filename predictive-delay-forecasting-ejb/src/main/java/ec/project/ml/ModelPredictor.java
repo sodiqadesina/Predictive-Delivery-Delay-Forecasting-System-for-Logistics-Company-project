5418c388f0a1837ce525dabce9c517132a93c66d
sina <sina@DESKTOP-KJM2ALJ>
2024-12-15 18:08:29 -0500

    ejb-pom fix

--- predictive-delay-forecasting-ejb/src/main/java/ec/project/ml/ModelPredictor.java
+++ predictive-delay-forecasting-ejb/src/main/java/ec/project/ml/ModelPredictor.java
@@ -1,49 +1,49 @@
-package ec.project.ml;
-
-import ec.project.dao.ModelDAO;
-import ec.project.model.ModelMetadata;
-import nz.ac.waikato.cms.weka.classifiers.Classifier;
-import nz.ac.waikato.cms.weka.core.Instance;
-import nz.ac.waikato.cms.weka.core.Instances;
-import nz.ac.waikato.cms.weka.core.converters.ConverterUtils.DataSource;
-
-import javax.ejb.Stateless;
-import javax.inject.Inject;
-import java.io.FileInputStream;
-import java.io.ObjectInputStream;
-
-@Stateless
-public class ModelPredictor {
-
-    @Inject
-    private ModelDAO modelDAO;
-
-    public double predict(String modelName, String inputDataPath) {
-        try {
-            // Get model metadata
-            ModelMetadata metadata = modelDAO.getModelByName(modelName);
-            if (metadata == null) {
-                throw new RuntimeException("Model not found: " + modelName);
-            }
-
-            // Load the model from file
-            Classifier model;
-            try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(metadata.getPath()))) {
-                model = (Classifier) in.readObject();
-            }
-
-            // Load input data
-            DataSource source = new DataSource(inputDataPath);
-            Instances inputData = source.getDataSet();
-            inputData.setClassIndex(inputData.numAttributes() - 1);
-
-            // Perform prediction for the first instance
-            Instance instance = inputData.firstInstance();
-            return model.classifyInstance(instance);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            return Double.NaN; // Indicate an error
-        }
-    }
-}
+//package ec.project.ml;
+//
+//import ec.project.dao.ModelDAO;
+//import ec.project.model.ModelMetadata;
+//import nz.ac.waikato.cms.weka.classifiers.Classifier;
+//import nz.ac.waikato.cms.weka.core.Instance;
+//import nz.ac.waikato.cms.weka.core.Instances;
+//import nz.ac.waikato.cms.weka.core.converters.ConverterUtils.DataSource;
+//
+//import javax.ejb.Stateless;
+//import javax.inject.Inject;
+//import java.io.FileInputStream;
+//import java.io.ObjectInputStream;
+//
+//@Stateless
+//public class ModelPredictor {
+//
+//    @Inject
+//    private ModelDAO modelDAO;
+//
+//    public double predict(String modelName, String inputDataPath) {
+//        try {
+//            // Get model metadata
+//            ModelMetadata metadata = modelDAO.getModelByName(modelName);
+//            if (metadata == null) {
+//                throw new RuntimeException("Model not found: " + modelName);
+//            }
+//
+//            // Load the model from file
+//            Classifier model;
+//            try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(metadata.getPath()))) {
+//                model = (Classifier) in.readObject();
+//            }
+//
+//            // Load input data
+//            DataSource source = new DataSource(inputDataPath);
+//            Instances inputData = source.getDataSet();
+//            inputData.setClassIndex(inputData.numAttributes() - 1);
+//
+//            // Perform prediction for the first instance
+//            Instance instance = inputData.firstInstance();
+//            return model.classifyInstance(instance);
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            return Double.NaN; // Indicate an error
+//        }
+//    }
+//}
