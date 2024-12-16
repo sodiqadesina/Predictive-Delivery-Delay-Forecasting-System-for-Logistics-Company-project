e995b91c3cd8b43eb8ec30d6f3935e97887e2028
sina <sina@DESKTOP-KJM2ALJ>
2024-12-15 19:33:34 -0500

    .

--- predictive-delay-forecasting-ejb/src/main/java/ec/project/ml/WekaUtils.java
+++ predictive-delay-forecasting-ejb/src/main/java/ec/project/ml/WekaUtils.java
@@ -1,34 +1,34 @@
-//package ec.project.ml;
-//
-//import nz.ac.waikato.cms.weka.classifiers.Classifier;
-//import nz.ac.waikato.cms.weka.classifiers.Evaluation;
-//import nz.ac.waikato.cms.weka.core.Instances;
-//import nz.ac.waikato.cms.weka.core.converters.ConverterUtils.DataSource;
-//
-//import java.util.Random;
-//
-//public class WekaUtils {
-//
-//    public static Instances loadDataset(String path) {
-//        try {
-//            DataSource source = new DataSource(path);
-//            Instances data = source.getDataSet();
-//            data.setClassIndex(data.numAttributes() - 1); // Set the last attribute as the class
-//            return data;
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            return null;
-//        }
-//    }
-//
-//    public static double evaluateModel(Classifier model, Instances data) {
-//        try {
-//            Evaluation evaluation = new Evaluation(data);
-//            evaluation.crossValidateModel(model, data, 10, new Random(1)); // 10-fold cross-validation
-//            return evaluation.pctCorrect(); // Percentage of correctly classified instances
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            return Double.NaN;
-//        }
-//    }
-//}
+package ec.project.ml;
+
+import nz.ac.waikato.cms.weka.classifiers.Classifier;
+import nz.ac.waikato.cms.weka.classifiers.Evaluation;
+import nz.ac.waikato.cms.weka.core.Instances;
+import nz.ac.waikato.cms.weka.core.converters.ConverterUtils.DataSource;
+
+import java.util.Random;
+
+public class WekaUtils {
+
+    public static Instances loadDataset(String path) {
+        try {
+            DataSource source = new DataSource(path);
+            Instances data = source.getDataSet();
+            data.setClassIndex(data.numAttributes() - 1); // Set the last attribute as the class
+            return data;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static double evaluateModel(Classifier model, Instances data) {
+        try {
+            Evaluation evaluation = new Evaluation(data);
+            evaluation.crossValidateModel(model, data, 10, new Random(1)); // 10-fold cross-validation
+            return evaluation.pctCorrect(); // Percentage of correctly classified instances
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Double.NaN;
+        }
+    }
+}
